7b242201ed0a816d4dddfd5ea088a384c5d47921
Seit Sorra <ssorra@phoenix-geophysics.com>
2019-10-01 09:04:50 -0400

    Added a new algorithm

---
+++ C++/src/powerSet.cpp
@@ -0,0 +1,18 @@
+/*
+    Power Set
+
+    Write a function that takes in an array of unique integers and returns its power set.
+    The power set P(x) of a set X is the set of all subsets of X.
+
+    For example the powerset of [1,2] is [[],[1],[2],[1,2]]
+    NOTE: the sets in the powerset do not need to be in any particular order
+
+    Sample Input: [1,2,3]
+    Sample Output: [[],[1],[2],[3],[1,2],[1,3],[1,2,3]]
+ */
+
+#include "utils.h"
+
+vector<vector<int>> powerSet(vector<int> array){
+    
+}
