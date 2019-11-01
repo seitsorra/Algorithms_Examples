1f8e65ad052f4bd2cf8fb2dc791100dd3da28dcf
Seit Sorra <ssorra@phoenix-geophysics.com>
2019-11-01 12:23:37 -0400

    Added another version of algorithm to solve permutations faster

--- C++/src/medium/permutations.cpp
+++ C++/src/medium/permutations.cpp
@@ -10,35 +10,55 @@
 */
 #include "../utils.h"
 
-vector<int> removeNum(int num, const vector<int>& arr){
-    vector<int> ret;
-
-    for(auto val : arr){
-        if(num != val){
-            ret.push_back(val);
-        }
-    }
-    return ret;
+void swap(int idx1, int idx2, vector<int>* array){
+    int tmp = array->at(idx1);
+    array->at(idx1) = array->at(idx2);
+    array->at(idx2) = tmp;
 }
 
-void helper(vector<int> arr, vector<int> permutation, vector<vector<int>>& perms){
+// This helper function will be called recursively that creates a permutation set for each index of the array
+// Upper Bound : O(n^2 * n!) time | O(n*n!) space
+// Roughly     : O(n*n!) time | O(n*n!) space
+void permutationHelper(vector<int> arr, vector<int> permutation, vector<vector<int>>& perms){
+    // When we hit an empty array then we have got a permutation set, so update the list
     if(arr.empty()){
         perms.push_back(permutation);
     }
+    // Otherwise for each index of the array, copy all elements excpect the current index into a new array
+    // and add the current index value to the permutation set (need to copy it because it is a recursive function)
     else{
         for(int idx = 0; idx < arr.size(); idx++){
-            vector<int> newArr = removeNum(arr.at(idx), arr);
+            vector<int> newArr;
+            newArr.insert(newArr.end(), arr.begin(), arr.begin() + idx);
+            newArr.insert(newArr.end(), arr.begin() + idx + 1, arr.end());
+
             vector<int> newPermutation = permutation;
             newPermutation.push_back(arr.at(idx));
-            helper(newArr, newPermutation, perms);
+
+            permutationHelper(newArr, newPermutation, perms);
+        }
+    }
+}
+
+// O(n*n!) time | O(n*n!) space
+void permutationHelperInPlace(int i, vector<int>* array, vector<vector<int>>* perms){
+    if(i == array->size()-1){
+        perms->push_back(*array);
+    }
+    else
+    {
+        for(int j = i; j < array->size(); j++){
+            swap(i, j, array);
+            permutationHelperInPlace(i+1, array, perms);
+            swap(i, j, array);
         }
     }
 }
 
 vector<vector<int>> getPermutations(vector<int> arr){
-    int staticIdx = 0;
     vector<vector<int>> ret;
-    helper(arr, {}, ret);
+    permutationHelperInPlace(0, &arr, &ret);
+    //permutationHelper(arr, {}, ret);
     return ret;
 }
 
@@ -59,6 +79,15 @@ int main(){
 
     for (int i = 0; i < cases.size(); i++) {
         vector<vector<int>> ret = getPermutations(cases[i].input);
+        for(auto it : ret){
+            std::sort(it.begin(), it.end());
+        }
+        for(auto it : test.result){
+            std::sort(it.begin(), it.end());
+        }
+        std::sort(ret.begin(), ret.end());
+        std::sort(test.result.begin(), test.result.end());
+
         if(ret == cases[i].result){
             cout << "Test Case " << i+1 << " PASSED!" << endl;
         }
